ad618e1e043835321c878ce429ed7c7424f1407a
Bhrevelec <166656993+Bhrevelec@users.noreply.github.com>
2024-05-21 17:46:11 +0200

    added user input for grid size

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //global variables
 const container = document.querySelector(".container");
 const values = document.querySelectorAll(".value");
+const btn = document.querySelector("button");
 
+//Ask user input on size when they request a new grid
+btn.addEventListener("click", () => {
+  const userInput = prompt(
+    "How many squares per side should the grid contain?"
+  );
+  if (userInput && userInput < 100) {
+    removeAllChildNodes(container);
+    createGrid(userInput);
+  }
+});
+
+//Clear all child nodes from a parent
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
+//Creates a grid of variable size inside the container div
 const createGrid = (size) => {
   //First create as many divs as there are columns
   //Inside those divs, create as many divs as there are rows
@@ -12,7 +32,7 @@ const createGrid = (size) => {
     for (let j = 1; j <= size; j++) {
       let horizontalDiv = document.createElement("div");
       horizontalDiv.setAttribute("class", "value");
-      horizontalDiv.style.border = "2px solid black";
+      horizontalDiv.style.border = "1px solid black";
       //horizontalDiv.innerText = i + "+" + j;
       verticalDiv.appendChild(horizontalDiv);
       //Add event listener to check if mouse hovers over a cell
@@ -23,4 +43,5 @@ const createGrid = (size) => {
   }
 };
 
+//Startup grid
 createGrid(16);
